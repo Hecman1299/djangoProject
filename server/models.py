e4dfa448aee26689e802763f87a060206e4237ea
Hecman1299 <loaxacaleon@gmail.com>
2024-06-14 09:19:50 -0600

    Initial commit

---
+++ server/models.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='servers_owner')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='servers_category')
+    description = models.CharField(max_length=250, null=True, blank=True)
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='servers_member')
+
+    def __str__(self):
+        return self.name
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channels_owner')
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channels_server')
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
